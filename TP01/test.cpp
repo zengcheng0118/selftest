60a9e5788037fe066428dcbd46dfcf9c101cdc8f
zengcheng0118 <954290837@qq.com>
2017-12-06 00:36:20 +0800

    TP01

---
+++ TP01/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "test.h"
+
+int main(int argc, char* argv[])
+{
+    int a, b;
+
+    using namespace std;
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+    cout << "Tutorial_VERSION_MAJOR: " << Tutorial_VERSION_MAJOR << endl;
+    cout << "Tutorial_VERSION_MINOR: " << Tutorial_VERSION_MINOR << endl;
+
+    return 0;
+}
