8b8dad909340945af2a07593022b566dd31df982
zengcheng0118 <954290837@qq.com>
2018-01-01 18:14:49 +0800

    init cpp porject

---
+++ cpp/build/CMakeFiles/cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp.dir/src/main.cpp.obj"
+  "cpp.pdb"
+  "cpp.exe"
+  "cpp.exe.manifest"
+  "libcpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    using std::cout;
+    using std::endl;
+
+    cout << "just test cpp" << endl;
+    
+    return 0;
+}
